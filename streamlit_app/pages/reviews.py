ac92791152d45db94e635e431f9bd0207ab7cf6a
1008tommy <darrenchor1832@gmail.com>
2026-08-19 19:36:23 +0800

    Update reviews.py

--- streamlit_app/pages/reviews.py
+++ streamlit_app/pages/reviews.py
@@ -1483,7 +1483,7 @@ MODEL_RUN_ID = "2472d4ed15474268a3d27cc2a880f395"
 
 MODEL_URI = (
     f"runs:/{MODEL_RUN_ID}/"
-    "tuned_modernbert_expanded_v2"
+    "modernbert_expanded_tuned_v2"
 )
 
 PREDICTION_THRESHOLD = 0.50
